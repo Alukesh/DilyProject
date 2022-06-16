cd94a93451ae1a4d217492c8a7882d38294bed25
Alukesh <akenzhebaev422@gmail.com>
2022-06-16 18:42:43 +0600

    '12345678'

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import {Provider} from "react-redux";
 import store from "./redux";
 import {BrowserRouter} from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('sabina'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
       <Provider store={store}>
           <BrowserRouter>
