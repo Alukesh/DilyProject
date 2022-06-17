1e70a1aef46f1c5282403748929407894e199451
Alukesh <akenzhebaev422@gmail.com>
2022-06-17 21:05:59 +0600

    online page visual styles

--- src/pages/Home/Compilation/CompilationRow/CompilationRow.js
+++ src/pages/Home/Compilation/CompilationRow/CompilationRow.js
@@ -1,11 +1,8 @@
 import React from 'react';
-import phone1 from "../phone1.png";
 import face1 from "../face.png";
-import phone2 from "../phone2.png";
-import phone3 from "../phone3.png";
 import CompilationCard from "../CompilationCard/CompilationCard";
 
-const CompilationRow = ({products, compilationCount = 5, countInRow, creator}) => {
+const CompilationRow = ({products, compilationCount = 5, sell, countInRow, creator}) => {
 
 
 
@@ -15,13 +12,13 @@ const CompilationRow = ({products, compilationCount = 5, countInRow, creator}) =
                 creator ?
                 products.filter(item => item.creator === creator).filter((el, idx)=> idx < compilationCount ).map(item =>(
                     <>
-                        <CompilationCard countInRow={countInRow} title={item.title} price={item.price} img={item.image} city={item.city} creator={face1}/>
+                        <CompilationCard sell={sell} countInRow={countInRow} title={item.title} price={item.price} img={item.image} city={item.city} creator={face1}/>
                     </>
                 ))
                     :
                     products.filter((el, idx)=> idx < compilationCount ).map(item =>(
                         <>
-                            <CompilationCard countInRow={countInRow} title={item.title} price={item.price} img={item.image} city={item.city} creator={face1}/>
+                            <CompilationCard sell={sell} countInRow={countInRow} title={item.title} price={item.price} img={item.image} city={item.city} creator={face1}/>
                         </>
                     ))
 
--- src/pages/Online/Online.js
+++ src/pages/Online/Online.js
@@ -1,6 +1,5 @@
 import React, {useState} from 'react';
-import "swiper/css";
-import "swiper/css/pagination";
+import { Swiper, SwiperSlide } from "swiper/react";
 import cash1 from "../../pages/Board/Banner/5.svg";
 import cash2 from "../../pages/Board/Banner/3.svg";
 import brand1 from './apple.png'
@@ -9,10 +8,18 @@ import brand3 from './mi.png'
 import brand4 from './honor.png'
 import brand5 from './sony.png'
 import brand6 from './tefal.png'
+import slide1 from './slide1.png'
 
 import OnlineBanner from "./OnlineBanner/OnlineBanner";
 import CompilationRow from "../Home/Compilation/CompilationRow/CompilationRow";
 import {useSelector} from "react-redux";
+import {Autoplay, Keyboard, Mousewheel, Navigation} from "swiper";
+import "swiper/css/navigation";
+import "swiper/css";
+import "swiper/css/pagination";
+import img2 from "../Board/Slider/img/2.svg";
+import img3 from "../Board/Slider/img/3.svg";
+
 
 
 
@@ -31,13 +38,13 @@ const Online = () => {
                         <h2>Хиты продаж</h2>
                         <span className={'compilation__show_all'} onClick={() => setMore(more + 5 < products.length ? more + 5 : 5 )}>{more + 5 < products.length ? 'Смотреть все' : 'убрать все' }</span>
                     </div>
-                    <CompilationRow products={products} compilationCount={more} creator={''}/>
+                    <CompilationRow sell={true} products={products} compilationCount={more} creator={''}/>
 
                     <div className={'compilation__chapter'}>
                         <h2>Новинки</h2>
                         <span className={'compilation__show_all'}>Смотреть все</span>
                     </div>
-                    <CompilationRow products={products} creator={1}/>
+                    <CompilationRow sell={true} products={products} creator={1}/>
 
                 </div>
             </div>
@@ -56,7 +63,45 @@ const Online = () => {
                     <h2>Скидки</h2>
                     <span className={'compilation__show_all'}>Смотреть все</span>
                 </div>
-                <CompilationRow products={products} />
+                <CompilationRow sell={true} products={products} />
+            </div>
+
+
+
+            <div className='online__slider'>
+                <div className="container">
+                    <Swiper
+                        slidesPerView={1}
+                        spaceBetween={30}
+                        centeredSlides={true}
+                        autoplay={{
+                            delay: 25000,
+                            disableOnInteraction: false,
+                        }}
+                        loop={true}
+                        autoPlay={true}
+                        keyboard={true}
+                        navigation={true}
+                        modules={[Navigation, Mousewheel, Keyboard, Autoplay]}
+                        className="mySwiper"
+                    >
+                        <SwiperSlide>
+                            <div className={'shadow-box online__slide online__slide1'}>
+                                <div className={'online__slide-info'}>
+                                    <p className={'online__slide-title'}>Новый IPhon 12</p>
+                                    <p className={'online__slide-subtitle'}>Теперь в новых цветах</p>
+                                    <p className={'online__slide-text'}>Уже в продаже</p>
+                                    <button className={'online__slide-btn greenBtn'}>Купить</button>
+                                </div>
+                                <img className={'online__slide-img greenBtn'} src={slide1} alt="slideImg"/>
+                            </div>
+                        </SwiperSlide>
+
+                        <SwiperSlide><div><img src={img2} alt="img"/><p className='slider__category'>Телефоны и аксессуары</p></div></SwiperSlide>
+                        <SwiperSlide><div><img src={img3} alt="img"/><p className='slider__category'>Электроника</p></div></SwiperSlide>
+                    </Swiper>
+                </div>
+
             </div>
 
 
@@ -68,26 +113,36 @@ const Online = () => {
                 </div>
                 <div className={'online__brands'}>
                     <div className={'online__brands-card shadow-box'}>
-                        <img src={brand1} alt=""/>
+                        <img className={'online__brands-img'} src={brand1} alt=""/>
                     </div>
                     <div className={'online__brands-card shadow-box'}>
-                        <img src={brand2} alt=""/>
+                        <img className={'online__brands-img'} src={brand2} alt=""/>
                     </div>
                     <div className={'online__brands-card shadow-box'}>
-                        <img src={brand3} alt=""/>
+                        <img className={'online__brands-img'} src={brand3} alt=""/>
                     </div>
                     <div className={'online__brands-card shadow-box'}>
-                        <img src={brand4} alt=""/>
+                        <img className={'online__brands-img'} src={brand4} alt=""/>
                     </div>
                     <div className={'online__brands-card shadow-box'}>
-                        <img src={brand5} alt=""/>
+                        <img className={'online__brands-img'} src={brand5} alt=""/>
                     </div>
                     <div className={'online__brands-card shadow-box'}>
-                        <img src={brand6} alt=""/>
+                        <img className={'online__brands-img'} src={brand6} alt=""/>
                     </div>
                 </div>
 
             </div>
+
+
+            <div className="container">
+                <div className={'compilation__chapter'}>
+                    <h2>Популярные смартфоны</h2>
+                    <span className={'compilation__show_all'}>Смотреть все</span>
+                </div>
+                <CompilationRow sell={true} products={products} />
+            </div>
+
 
 
 
